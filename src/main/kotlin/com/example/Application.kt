0ab8adebfbb1079126bc1f3edd0a2e6299a2cd1a
arjeshvadadoriya4052 <arjesh.vadadoriya@grewon.com>
2024-03-14 18:14:48 +0530

    latest code

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.example.dao.DatabaseSingleton
 import com.example.plugins.*
 import io.ktor.server.application.*
 
@@ -8,6 +9,7 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    DatabaseSingleton.init()
     configureTemplating()
     configureRouting()
 }
--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
@@ -9,12 +10,6 @@ import io.ktor.server.routing.*
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
-    }
-    routing {
-        get("/html-freemarker") {
-            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
-        }
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
 }
-
-data class IndexData(val items: List<Int>)
